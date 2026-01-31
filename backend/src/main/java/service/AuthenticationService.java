88314af0477cb5dc715f6fae1ba46a9871744b6a
gabres96 <gabriel.rodrigues670@gmail.com>
2026-01-30 23:06:01 -0300

    feat: add authentication controller, service and DTOs

---
+++ backend/src/main/java/service/AuthenticationService.java
@@ -0,0 +1,41 @@
+package service;
+
+import dto.LoginRequest;
+import dto.RegisterRequest;
+import entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterRequest registerRequest) {
+        if (userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
+            throw new RuntimeException("O E-mail já existe");
+        }
+
+        User user = User.builder()
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .build();
+        userRepository.save(user);
+    }
+    public void login(LoginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(()-> new RuntimeException("Credenciais invalidas"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Credenciais invalidas");
+        }
+    }
+}
